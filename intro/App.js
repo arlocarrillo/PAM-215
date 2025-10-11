e57cc2c124b493336632348560fea9751e1402aa
arlocarrillo <124049071@upq.edu.mx>
2025-10-10 20:14:28 -0600

    Creación de la vista

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+//imports: Zona de importaciones
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
+
+//Main: Zona de componentes
 export default function App() {
+  const[contador,setContador]=useState(0);
   return (
     <View style={styles.container}>
-      <Text>Hola mundo React Native</Text>
+      <Text>Contador: {contador}</Text>
+      <Button title='Incrementar' onPress={()=>setContador(contador+1)}></Button>
+      <Button title='Quitar' onPress={()=>setContador(contador-1)}></Button>
+      <Button title='Reiniciar' onPress={()=>setContador(0)}></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//Estilos: Zona de los estilos y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
