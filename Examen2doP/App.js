9347c1a53baeb97402423dd921b94a0497bd2b24
arlocarrillo <124049071@upq.edu.mx>
2025-11-07 20:30:26 -0600

    Update App.js

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -3,7 +3,9 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, ScrollView, ImageBackground, Switch, Button } from 'react-native';
 
 export default function App() {
-  const[estatus, setEstatus] = useState(false);
+  const[escuela, setEscuela] = useState(false);
+  const[hogar, setHogar] = useState(false);
+  const[trabajo, setTrabajo] = useState(false);
   return (
     <ImageBackground source={('')} style={styles.container}>
       <View>
@@ -13,15 +15,38 @@ export default function App() {
       </View>
       <ScrollView style={styles.contenedorScroll}>
         <Text style={styles.titulo}>Categorías</Text>
+
         <View style={styles.botones}>
-        <Button title="Estudios" style={styles.botones} color='#020c7bff'/>
+        <Text style={styles.fecha}>Escuela</Text>
+        <Text>Práctica 14 de React Native</Text>
+        <Switch
+          value={escuela}
+          onValueChange={() => setEscuela(!escuela)}
+        />
+        <Text style={{color:escuela ? color:'red'}}>{escuela ? 'Completado' : 'Pendiente'}</Text>
         </View>
-        <View style={styles.botones}>
-        <Button title="Hogar" style={styles.botones} color='#8b257cff'/>
+
+        <View style={styles.hogar}>
+        <Text style={styles.fecha}>Hogar</Text>
+        <Text>Lavar los trastes</Text>
+        <Switch
+          value={hogar}
+          onValueChange={() => setHogar(!hogar)}
+        />
+        <Text style={[{color:hogar ? color:'red'}]}>{hogar ? 'Completado' : 'Pendiente'}</Text>
         </View>
-        <View style={styles.botones}>
-        <Button title="Trabajo" style={styles.botones} color='#187703ff'/>
+
+        <View style={styles.trabajo}>
+        <Text style={styles.fecha}>Trabajo</Text>
+        <Text>Realizar negativos</Text>
+        <Switch
+          value={trabajo}
+          onValueChange={() => setTrabajo(!trabajo)}
+        />
+        <Text style={{color:trabajo ? color:'red'}}>{trabajo ? 'Completado' : 'Pendiente'}</Text>
         </View>
+
+        <Button title='Reiniciar' onPress={{}}/>
       </ScrollView>
     </ImageBackground>
   );
@@ -56,7 +81,29 @@ const styles = StyleSheet.create({
     backgroundColor:'gray',
   },
   botones:{
-    marginTop:10,
-    marginBottom:10,
+    marginTop:50,
+    marginBottom:50,
+    height:30,
+    width:'100%',
+    backgroundColor:'#61dafb',
+  },
+  hogar:{
+    marginTop:50,
+    marginBottom:50,
+    height:30,
+    width:'100%',
+    backgroundColor:'#ffb6c1',
+  },
+  trabajo:{
+    marginTop:50,
+    marginBottom:80,
+    height:30,
+    width:'100%',
+    backgroundColor:'#90ee90',
+  },
+  estado:{
+    marginTop:20,
+    fontSize:20,
+    fontWeight:'bold',
   }
 });
